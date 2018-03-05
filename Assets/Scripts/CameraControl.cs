e0aaca1ace28cc6518c4e8761159c45bd219f6d5
Ana Mendes <anamendes23@gmail.com>
2018-03-04 19:48:56 -0800

    First commit

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public GameObject player;
+
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start ()
+    {
+        offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+	}
+    //garantees that the player moved before the camera
+    private void LateUpdate() //run after all process have been processed in update
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    //public variables can be change in the editor
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int count;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = string.Empty;
+    }
+    private void Update() //called before rendering a frame
+    {
+        
+    }
+
+    private void FixedUpdate() //called before performing any physics calculation
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal"); //input from user
+        float moveVertical = Input.GetAxis("Vertical"); //input from user
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        //Destroy(other.gameObject);
+        //if (other.gameObject.CompareTag("Player"))
+        //    gameObject.SetActive(false);
+        if(other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    //function
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 12)
+        { winText.text = "You win!"; }
+    }
+}
